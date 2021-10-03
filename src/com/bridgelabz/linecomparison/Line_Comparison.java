b7e304a6a729f31d5a65c455902063d2d3402cc2
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-03 17:51:35 +0530

    Methods to Compare length of two lines

--- src/com/bridgelabz/linecomparison/Line_Comparison.java
+++ src/com/bridgelabz/linecomparison/Line_Comparison.java
@@ -3,18 +3,18 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class Line_Comparison{
-	public static int x1, x2, x3, x4, y1, y2, y3, y4;
-	public static double distance1, distance2;
+	public  int x1, x2, x3, x4, y1, y2, y3, y4;
+	public  double distance1, distance2;
 
 	public static void main(String[] args) {
 		Line_Comparison linecomparison=new Line_Comparison();
 		linecomparison.welcome();
 		linecomparison.userInput();
 		linecomparison.distance();
-		linecomparison.equal();
+		linecomparison.compareTo();
 	}
 
-	 public void welcome() {
+	public void welcome() {
 		System.out.println("Welcome to Line Comparsion Computation Program");
 	}
 
@@ -50,7 +50,7 @@ public class Line_Comparison{
 		sc.close();
 	}
 
-	public   void distance() {
+	public  void distance() {
 		distance1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		distance2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
 
@@ -58,12 +58,13 @@ public class Line_Comparison{
 		System.out.println("Distance 2 :" + distance2);
 	}
 
-	public   void equal() {
+	public  void compareTo() {
 		if (distance1 == distance2) {
 			System.out.println("Length of two lines are equal");
+		} else if (distance1 > distance2) {
+			System.out.println("Length of line 1 is greater than line 2");
 		} else {
-			System.out.println("Length of two lines are not equal");
+			System.out.println("Length of line 2 is greater than line 1");
 		}
 	}
-
 }
