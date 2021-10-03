9c7f47a0e2f3f4894a08ade59d043c5812572e9a
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-03 17:28:45 +0530

    Methods to find Equality of two lines

--- src/com/bridgelabz/linecomparison/Line_Comparison.java
+++ src/com/bridgelabz/linecomparison/Line_Comparison.java
@@ -3,22 +3,25 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class Line_Comparison{
-	public static int x1, x2, y1, y2;
-	public static double distance;
+	public static int x1, x2, x3, x4, y1, y2, y3, y4;
+	public static double distance1, distance2;
 
 	public static void main(String[] args) {
 		Line_Comparison linecomparison=new Line_Comparison();
 		linecomparison.welcome();
 		linecomparison.userInput();
 		linecomparison.distance();
+		linecomparison.equal();
 	}
 
-	public void welcome() {
+	 public void welcome() {
 		System.out.println("Welcome to Line Comparsion Computation Program");
 	}
 
 	public  void userInput() {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("For line 1");
+
 		System.out.println("Enter the value of X1");
 		x1 = sc.nextInt();
 
@@ -30,12 +33,37 @@ public class Line_Comparison{
 
 		System.out.println("Enter the value of Y2");
 		y2 = sc.nextInt();
+
+		System.out.println("For line 2");
+
+		System.out.println("Enter the value of X1");
+		x3 = sc.nextInt();
+
+		System.out.println("Enter the value of X2");
+		x4 = sc.nextInt();
+
+		System.out.println("Enter the value of Y1");
+		y3 = sc.nextInt();
+
+		System.out.println("Enter the value of Y2");
+		y4 = sc.nextInt();
 		sc.close();
 	}
 
-	public  void distance() {
-		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Length of a line : " + +distance);
+	public   void distance() {
+		distance1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		distance2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+
+		System.out.println("Distance 1 :" + distance1);
+		System.out.println("Distance 2 :" + distance2);
+	}
+
+	public   void equal() {
+		if (distance1 == distance2) {
+			System.out.println("Length of two lines are equal");
+		} else {
+			System.out.println("Length of two lines are not equal");
+		}
 	}
 
 }
