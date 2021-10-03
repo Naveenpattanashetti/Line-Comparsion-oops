98f30652d3a8713d083d188d981b95bd06ac340b
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-03 16:59:11 +0530

    added:gitignore file

---
+++ src/com/bridgelabz/linecomparison/Line_Comparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparison;
+
+public class Line_Comparison {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparison problem");
+
+	}
+
+}
